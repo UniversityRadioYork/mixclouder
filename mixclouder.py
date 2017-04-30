d97b682382797ecf5a9ecca9cc8d49baa41b901e
Charles Pigott <charlespigott@googlemail.com>
2017-04-30 12:51:32 +0100

    Fix indentation
    Why is this even necessary?! The top comment *claims* it used to be python3, but python3 wouldn't have touched this with a barge pole

--- mixclouder.py
+++ mixclouder.py
@@ -65,24 +65,24 @@ def get_duration(duration):
     return int(td.total_seconds())
 
 def loggerng_api_request(action, timeslot):
-  start_time = get_epoch(timeslot['start_time']+':00')+(int(config.get("mixclouder", "news_length")))
-  # Timeslots return start time relevant to local time at that point. If it was in dst, subtract an hour
-  if time.localtime(start_time).tm_isdst:
-    start_time -= 3600
-  end_time = start_time + get_duration(timeslot['duration'])
-  return requests.get(config.get("mixclouder", "loggerng_url") + action, params={'user': config.get("mixclouder", "loggerng_memberid"), 'start': start_time, 'end': end_time, 'format': 'mp3', 'title': timeslot['timeslot_id']})
+    start_time = get_epoch(timeslot['start_time']+':00')+(int(config.get("mixclouder", "news_length")))
+    # Timeslots return start time relevant to local time at that point. If it was in dst, subtract an hour
+    if time.localtime(start_time).tm_isdst:
+        start_time -= 3600
+    end_time = start_time + get_duration(timeslot['duration'])
+    return requests.get(config.get("mixclouder", "loggerng_url") + action, params={'user': config.get("mixclouder", "loggerng_memberid"), 'start': start_time, 'end': end_time, 'format': 'mp3', 'title': timeslot['timeslot_id']})
 
 def cleanse_description(id, desc):
-  # remove html tags
-  desc = re.sub('<[^<]+?>', '', desc)
-  # HTML unescape
-  desc = HTMLParser().unescape(desc)
-  # limit the length due to mixcloud api restrictions
-  if len(desc) > 1000:
-    desc = desc[:1000]
-    # just log a warning so we can manually change if need be
-    logging.warn("Timeslot %s description was too long and was trimmed", id)
-  return desc
+    # remove html tags
+    desc = re.sub('<[^<]+?>', '', desc)
+    # HTML unescape
+    desc = HTMLParser().unescape(desc)
+    # limit the length due to mixcloud api restrictions
+    if len(desc) > 1000:
+        desc = desc[:1000]
+        # just log a warning so we can manually change if need be
+        logging.warn("Timeslot %s description was too long and was trimmed", id)
+    return desc
 
 argparser = argparse.ArgumentParser(description="Takes recent shows and publishes them to Mixcloud")
 argparser.add_argument('-c', '--config-file', required=True)
@@ -103,55 +103,56 @@ config.read(args.config_file)
 # Logs are available for the last 14 all of those.
 log_start = int(time.mktime((datetime.datetime.now() + datetime.timedelta(-14)).timetuple()))
 if time.localtime(log_start).tm_isdst:
-  log_start -= 3600
+    log_start -= 3600
 timeslots = []
 while True:
-  logging.info("Start request" + str(log_start))
-  ts = myradio_api_request('Timeslot/getNextTimeslot/', {'time': log_start})
-  #ts returns None if there is no next timeslot (i.e. end of term).
-  if ts is None:
-    break
-  log_start = get_epoch(ts['start_time']+':01')
-  if time.localtime(log_start).tm_isdst:
-    log_start -= 3600
-  logging.info("Updated Start request" + str(log_start))
-  logging.info(ts['start_time'])
-  logging.info(ts['mixcloud_status'])
-  if log_start + get_duration(ts['duration']) > time.time():
-    break
-  #Check if this show is opted in to logging and hasn't already been done
-  if ts['mixcloud_status'] == 'Requested':
-    # Was something other than jukebox on air at the time? (well, 2.5m in)
-    if myradio_api_request('Selector/getStudioAtTime/', {'time': log_start+150}) != 3:
-      timeslots.append(ts)
-      myradio_api_request('Timeslot/'+str(ts['timeslot_id'])+'/setMeta/', 
-{'string_key': 'upload_state', 'value': 'Queued'}, method = "POST")
-    else:
-      logging.warn("Timeslot "+str(ts['timeslot_id'])+" was not on air!")
-      myradio_api_request('Timeslot/'+str(ts['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': 'Skipped - Off Air'}, method = "POST")
+    logging.info("Start request" + str(log_start))
+    ts = myradio_api_request('Timeslot/getNextTimeslot/', {'time': log_start})
+    #ts returns None if there is no next timeslot (i.e. end of term).
+    if ts is None:
+        break
+    log_start = get_epoch(ts['start_time']+':01')
+    if time.localtime(log_start).tm_isdst:
+        log_start -= 3600
+    logging.info("Updated Start request" + str(log_start))
+    logging.info(ts['start_time'])
+    logging.info(ts['mixcloud_status'])
+    if log_start + get_duration(ts['duration']) > time.time():
+        break
+    #Check if this show is opted in to logging and hasn't already been done
+    if ts['mixcloud_status'] == 'Requested':
+        # Was something other than jukebox on air at the time? (well, 2.5m in)
+        if myradio_api_request('Selector/getStudioAtTime/', {'time': log_start+150}) != 3:
+            timeslots.append(ts)
+            myradio_api_request('Timeslot/'+str(ts['timeslot_id'])+'/setMeta/',
+                {'string_key': 'upload_state', 'value': 'Queued'}, method = "POST")
+        else:
+            logging.warn("Timeslot "+str(ts['timeslot_id'])+" was not on air!")
+            myradio_api_request('Timeslot/'+str(ts['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': 'Skipped - Off Air'}, method = "POST")
 
 logging.info("Found %s shows pending upload.", len(timeslots))
 
 for timeslot in timeslots:
-  #Skip ones that already have some kind of status, except queued
-  if timeslot['mixcloud_status'] != 'Requested':
-    logging.info("Skipping "+str(timeslot['timeslot_id'])+" as it does not need mixcloudifying.")
-    continue
+    #Skip ones that already have some kind of status, except queued
+    if timeslot['mixcloud_status'] != 'Requested':
+        logging.info("Skipping "+str(timeslot['timeslot_id'])+" as it does not need mixcloudifying.")
+        continue
 
-  tracklist = sorted(myradio_api_request('TracklistItem/getTracklistForTimeslot', {'timeslotid': timeslot['timeslot_id']}), key=lambda k: k['starttime'])
-  if len(tracklist) < 1:
-    logging.warn("Timeslot "+timeslot['title']+' '+str(timeslot['season_num'])+'x'+str(timeslot['timeslot_num'])+" does not have at least 1 track in its tracklist data")
-    myradio_api_request('Timeslot/'+str(timeslot['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': 'Skipped - Incomplete Tracklist'}, method = "POST")
-  else:
+    tracklist = sorted(myradio_api_request('TracklistItem/getTracklistForTimeslot', {'timeslotid': timeslot['timeslot_id']}), key=lambda k: k['starttime'])
+    if len(tracklist) < 1:
+        logging.warn("Timeslot "+timeslot['title']+' '+str(timeslot['season_num'])+'x'+str(timeslot['timeslot_num'])+" does not have at least 1 track in its tracklist data")
+        myradio_api_request('Timeslot/'+str(timeslot['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': 'Skipped - Incomplete Tracklist'}, method = "POST")
+        continue
+
     #Great, now let's make a request for the log file
     r = loggerng_api_request("make", timeslot)
     logging.info("Initiated log generation for timeslot %s", timeslot['timeslot_id'])
     #Wait until we can download it
     r = loggerng_api_request("download", timeslot)
     while r.status_code == 403:
-      logging.info("Still waiting for log generation (%s)", r.status_code)
-      time.sleep(30)
-      r = loggerng_api_request("download", timeslot)
+        logging.info("Still waiting for log generation (%s)", r.status_code)
+        time.sleep(30)
+        r = loggerng_api_request("download", timeslot)
     r = r.json() if callable (r.json) else r.json
     file = config.get("mixclouder", "loggerng_logdir") + '/' + r['filename_disk']
     # Okay, time to build request data
@@ -161,11 +162,11 @@ for timeslot in timeslots:
         'sections-0-start_time': 0,
         'sections-0-chapter': 'Top of Hour'
     }
-    
+
     # Add the tags, if they are a thing
     for i in range(min(5, len(timeslot['tags']))):
-      data['tags-'+str(i)+'-tag'] = timeslot['tags'][i]
-    
+        data['tags-'+str(i)+'-tag'] = timeslot['tags'][i]
+
     # For the perecentage_music field, we need to work out how much is speech and how much is... well, something else.
     # Let's start with the length of the show
     duration = get_duration(timeslot['duration'])
@@ -174,11 +175,11 @@ for timeslot in timeslots:
     sindex = 1
     # Now, for each song in the tracklist, we'll add the data to the tracklist headers, and also increment the music_time
     for i in tracklist:
-      data['sections-'+str(sindex)+'-artist'] = i['artist']
-      data['sections-'+str(sindex)+'-song'] = i['title']
-      data['sections-'+str(sindex)+'-start_time'] = get_epoch(i['starttime'])-get_epoch(timeslot['start_time']+':00')-(int(config.get("mixclouder", "news_length")))
-      sindex += 1
-      music_time += get_duration(str(i['length'])) if i['length'] else 0
+        data['sections-'+str(sindex)+'-artist'] = i['artist']
+        data['sections-'+str(sindex)+'-song'] = i['title']
+        data['sections-'+str(sindex)+'-start_time'] = get_epoch(i['starttime'])-get_epoch(timeslot['start_time']+':00')-(int(config.get("mixclouder", "news_length")))
+        sindex += 1
+        music_time += get_duration(str(i['length'])) if i['length'] else 0
     # Work out that percentage of music I mentioned earlier
     data['percentage_music'] = int(music_time/duration*100)
 
@@ -196,15 +197,15 @@ for timeslot in timeslots:
 
     # You don't want to know what happens to alpha channels
     if im.mode == "RGBA":
-      pixel_data = im.load()
-      # If the image has an alpha channel, convert it to white
-      # Otherwise we'll get weird pixels
-      for y in xrange(im.size[1]): # For each row ...
-        for x in xrange(im.size[0]): # Iterate through each column ...
-          # Check if it's opaque
-          if pixel_data[x, y][3] < 255:
-            # Replace the pixel data with the colour white
-            pixel_data[x, y] = (255, 255, 255, 255)
+        pixel_data = im.load()
+        # If the image has an alpha channel, convert it to white
+        # Otherwise we'll get weird pixels
+        for y in xrange(im.size[1]): # For each row ...
+            for x in xrange(im.size[0]): # Iterate through each column ...
+                # Check if it's opaque
+                if pixel_data[x, y][3] < 255:
+                    # Replace the pixel data with the colour white
+                    pixel_data[x, y] = (255, 255, 255, 255)
 
     xsize, ysize = im.size
     outsize = x_size if xsize > ysize else ysize
@@ -220,7 +221,7 @@ for timeslot in timeslots:
     del im, new
 
     files['picture'] = open(tmpname+".jpg", "rb")
-    
+
     logging.info("Starting upload of %s to Mixcloud", data['name'])
 
 #    print(data)
@@ -229,14 +230,14 @@ for timeslot in timeslots:
     r = requests.post('https://api.mixcloud.com/upload/?access_token='+config.get("mixclouder", "mixcloud_client_oauth"), data=data, files=files)
     info = r.json() if callable (r.json) else r.json
     if r.status_code != 200:
-      logging.error(info['error']['message'])
-      # Put the log back into the queue
-      myradio_api_request('Timeslot/'+str(timeslot['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': 'Requested'}, method = "POST")
-      # Wait before carrying on if it's an API limit
-      if 'retry_after' in info['error']:
-        logging.error('Waiting '+str(info['error']['retry_after'])+' seconds before continuing.')
-        time.sleep(info['error']['retry_after'])
+        logging.error(info['error']['message'])
+        # Put the log back into the queue
+        myradio_api_request('Timeslot/'+str(timeslot['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': 'Requested'}, method = "POST")
+        # Wait before carrying on if it's an API limit
+        if 'retry_after' in info['error']:
+            logging.error('Waiting '+str(info['error']['retry_after'])+' seconds before continuing.')
+            time.sleep(info['error']['retry_after'])
     else:
-      logging.info('Upload successful!')
-      myradio_api_request('Timeslot/'+str(timeslot['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': info['result']['key']}, method = "POST")
+        logging.info('Upload successful!')
+        myradio_api_request('Timeslot/'+str(timeslot['timeslot_id'])+'/setMeta/', {'string_key': 'upload_state', 'value': info['result']['key']}, method = "POST")
     print(r.content)
